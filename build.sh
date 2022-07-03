34cf32f614d593a70091f5698154af331bc705d1
albin <albin@albin-debian.fr>
2022-07-03 19:08:03 +0200

    Removed unused dependency from build.sh (ncurses)

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-CFLAGS="-std=c11 -ggdb `pkg-config --cflags --libs sdl2 ncurses` -lm"
-LIBS="`pkg-config --libs --static sdl2 ncurses` -lm"
+CFLAGS="-std=c11 -ggdb `pkg-config --cflags --libs sdl2` -lm"
+LIBS="`pkg-config --libs --static sdl2` -lm"
 
 
 gcc $CFLAGS -o pink-pine main.c $LIBS
